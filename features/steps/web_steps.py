5e20f5c28298da7335f3870cdbc7a055c420e297
Zhe Ye <113217365+hiddenyz@users.noreply.github.com>
2023-12-11 01:17:40 -0500

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -168,12 +168,17 @@ def step_impl(context, name):
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-    found = WebDriverWait(context.driver, context.wait_seconds).until(
-        expected_conditions.text_to_be_present_in_element(
-            (By.ID, "flash_message"), message
+    try:
+        found = WebDriverWait(context.driver, context.wait_seconds).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, "flash_message"), message
+            )
         )
-    )
-    assert found
+        assert found
+    except TimeoutException:
+        logging.error(f"Message '{message}' not found within {context.wait_seconds} seconds.")
+        raise
+
 
 
 ##################################################################
